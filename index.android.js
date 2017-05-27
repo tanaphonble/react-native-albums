2b24f663eec06c8c1f44253b878149f1d3b91a34
Tanaphon Auanhinkong <a.tanaphon@gmail.com>
2017-05-28 00:18:51 +0700

    create scroll view

--- index.android.js
+++ index.android.js
@@ -1,12 +1,24 @@
 // Import a library to help create a component
 import React from 'react'
-import { AppRegistry } from 'react-native'
+import { AppRegistry, View, Text } from 'react-native'
 import Header from './src/components/header'
+import AlbumList from './src/components/AlbumList'
 
 // Create a component 
+// const App = () => (
+//   <View>
+//     <Header headerText={'Albums'} />
+//     <AlbumList />
+//   </View>
+// )
+
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 )
+
 
 // Render it to device
 AppRegistry.registerComponent('albums', () => App)
